f71c8561ba1e5c8f468413fad50b7605c2432865
Lucio Rodriguez <luonstreak@gmail.com>
2018-01-16 18:04:36 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+var mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extend: true }));
+app.use(express.static(path.join(__dirname,'./static')));
+app.set('views', path.join(__dirname,'./views'));
+app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb://localhost/basic_mongoose');
+mongoose.Promise = global.Promise;
+
+app.get('/', function(req,res){
+	Monguser.find({}, function(err, mongusers){
+		if(err){
+			console.log("something went wrong trying to retrieve all mongooses");
+		} else {
+			console.log("mongooses found")
+			res.render('index', {data: mongusers});
+		}
+	})
+})
+app.get('style.css', function(req, res){
+	res.render('style.css');
+})
+app.get('/new', function(req,res){
+	res.render('new');
+})
+app.post('/create', function(req,res){
+	var monguser = new Monguser({
+		name: req.body.name,
+		height: req.body.height,
+		weight: req.body.weight
+	});
+	monguser.save(function(err){
+		if(err){
+			console.log("something went wrong saving the new monguser");
+		} else {
+			console.log("new monguser added!");
+			res.redirect('/');
+		} 
+	})
+})
+app.listen(8000, function(){
+	console.log('-- running localhost in port 8000 --');
+})
+
+var monguserSchema = new mongoose.Schema({
+	name: String,
+	height: Number,
+	weight: Number
+})
+mongoose.model('Monguser', monguserSchema);
+var Monguser = mongoose.model('Monguser');
